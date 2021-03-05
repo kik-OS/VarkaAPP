79473a1e0306bc837f53ceb56ee70842682c3375
Nikita Gvozdikov <pounddnuop@mail.ru>
2021-03-05 10:13:46 +0300

    initial app settings

---
+++ VarkaAPP/InfoAboutProducts/InfoAboutProductViewController.swift
@@ -0,0 +1,25 @@
+//
+//  InfoAboutProductViewController.swift
+//  VarkaAPP
+//
+//  Created by Никита Гвоздиков on 05.03.2021.
+//
+
+import UIKit
+
+
+class InfoAboutProductViewController: UIViewController {
+    
+    
+    private var viewModel: InfoAboutProductViewMOdelProtocol!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+
+
+    
+    
+}
