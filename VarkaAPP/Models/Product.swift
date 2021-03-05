3060280a347698e663174d69b2093762a40f056d
Evgeny Novgorodov <greatly01@ya.ru>
2021-03-05 23:38:34 +0500

    Model Product was added

---
+++ VarkaAPP/Models/Product.swift
@@ -0,0 +1,20 @@
+//
+//  Product.swift
+//  VarkaAPP
+//
+//  Created by Evgeny Novgorodov on 05.03.2021.
+//
+
+import Foundation
+
+struct Product {
+    let code: String
+    let title: String
+    let producer: String
+    let category: String
+    let weight: Int
+    let cookingTime: Int
+    let isNeedStir: Bool
+    let intoBoilingWater: Bool
+    let needStirring: Bool
+}
