56509577cbb7c325ea6c763677d86468fdd8dfce
Nikita Gvozdikov <pounddnuop@mail.ru>
2021-03-05 10:03:13 +0300

    Initial Commit

---
+++ VarkaAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VarkaAPP
+//
+//  Created by Никита Гвоздиков on 05.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
