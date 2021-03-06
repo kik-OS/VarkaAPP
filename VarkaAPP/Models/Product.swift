07f30f0d82a6c7503615c37b02b812245ad166f5
Evgeny Novgorodov <greatly01@ya.ru>
2021-03-06 12:05:59 +0500

    Model Product was updated

--- VarkaAPP/Models/Product.swift
+++ VarkaAPP/Models/Product.swift
@@ -13,12 +13,23 @@ struct Product {
     let title: String
     let producer: String
     let category: String
-    let cookingTime: Int
     let weight: Int?
-    let isNeedStir: Bool?
+    let cookingTime: Int
     let intoBoilingWater: Bool?
     let needStirring: Bool?
     let ref: DatabaseReference?
+    
+    init(code: String, title: String, producer: String, category: String, weight: Int?, cookingTime: Int, intoBoilingWater: Bool?, needStirring: Bool?, ref: DatabaseReference? = nil) {
+        self.code = code
+        self.title = title
+        self.producer = producer
+        self.category = category
+        self.cookingTime = cookingTime
+        self.weight = weight
+        self.intoBoilingWater = intoBoilingWater
+        self.needStirring = needStirring
+        self.ref = ref
+    }
     
     init?(snapshot: DataSnapshot) {
         guard let snapshotValue = snapshot.value as? [String: AnyObject] else { return nil }
@@ -27,11 +38,26 @@ struct Product {
         title = snapshotValue["title"] as! String
         producer = snapshotValue["producer"] as! String
         category = snapshotValue["category"] as! String
-        cookingTime = snapshotValue["cookingTime"] as! Int
         weight = snapshotValue["weight"] as? Int
-        isNeedStir = snapshotValue["isNeedStir"] as? Bool
+        cookingTime = snapshotValue["cookingTime"] as! Int
         intoBoilingWater = snapshotValue["intoBoilingWater"] as? Bool
         needStirring = snapshotValue["needStirring"] as? Bool
         ref = snapshot.ref
+    }
+}
+
+extension Product {
+    
+    static func getProducts() -> [Product] {
+        [Product(
+            code: "1234567890123", title: "Rice", producer: "Agro-Alliance",
+            category: "Grocery", weight: 800, cookingTime: 600,
+            intoBoilingWater: true, needStirring: false
+        ),
+        Product(
+            code: "9876543210987", title: "Buckwheat", producer: "Agro-Alliance",
+            category: "Grocery", weight: 900, cookingTime: 420,
+            intoBoilingWater: false, needStirring: false
+        )]
     }
 }
