c8ec2e70119aeb8a25b343d804133cd337a6eea8
Evgeny Novgorodov <greatly01@ya.ru>
2021-03-29 20:52:00 +0500

    UIView+Extension was updated

--- VarkaAPP/Additional Files /Extensions/UIView+Extension.swift
+++ VarkaAPP/Additional Files /Extensions/UIView+Extension.swift
@@ -9,7 +9,8 @@ import UIKit
 
 extension UIView {
     
-    func appear(fromValue: CGFloat = 0, toValue: CGFloat = 1, duration: Double = 0.4) {
+    func appear(fromValue: CGFloat = 0, toValue: CGFloat = 1,
+                duration: Double = 0.5, completion: @escaping (() -> Void) = {}) {
         isHidden = false
         alpha = fromValue
         UIView.animate(withDuration: duration) {
@@ -17,11 +18,13 @@ extension UIView {
         } completion: { isEnded in
             if isEnded {
                 self.alpha = toValue
+                completion()
             }
         }
     }
     
-    func disappear(fromValue: CGFloat = 1, toValue: CGFloat = 0, duration: Double = 0.4) {
+    func disappear(fromValue: CGFloat = 1, toValue: CGFloat = 0,
+                   duration: Double = 0.5, completion: @escaping (() -> Void) = {}) {
         alpha = fromValue
         UIView.animate(withDuration: duration) {
             self.alpha = toValue
@@ -29,6 +32,7 @@ extension UIView {
             if isEnded {
                 self.alpha = toValue
                 self.isHidden = true
+                completion()
             }
         }
     }
