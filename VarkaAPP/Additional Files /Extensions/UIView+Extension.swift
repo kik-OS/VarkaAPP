fb11a8bf4b0ddf4e0ffc36b38312ed1688ffb500
Evgeny Novgorodov <greatly01@ya.ru>
2021-03-29 11:14:17 +0500

    UIView+Extension was added

---
+++ VarkaAPP/Additional Files /Extensions/UIView+Extension.swift
@@ -0,0 +1,35 @@
+//
+//  UIView+Extension.swift
+//  VarkaAPP
+//
+//  Created by Evgeny Novgorodov on 28.03.2021.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func appear(fromValue: CGFloat = 0, toValue: CGFloat = 1, duration: Double = 0.4) {
+        isHidden = false
+        alpha = fromValue
+        UIView.animate(withDuration: duration) {
+            self.alpha = toValue
+        } completion: { isEnded in
+            if isEnded {
+                self.alpha = toValue
+            }
+        }
+    }
+    
+    func disappear(fromValue: CGFloat = 1, toValue: CGFloat = 0, duration: Double = 0.4) {
+        alpha = fromValue
+        UIView.animate(withDuration: duration) {
+            self.alpha = toValue
+        } completion: { isEnded in
+            if isEnded {
+                self.alpha = toValue
+                self.isHidden = true
+            }
+        }
+    }
+}
